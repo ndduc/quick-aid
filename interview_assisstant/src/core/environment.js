f6b4813ac0b5231d7778a42864686651da1bfbca
Duc Nguyen <ukk2@cdc.gov>
2025-09-02 15:08:29 -0700

    update environemnt

--- interview_assisstant/src/core/environment.js
+++ interview_assisstant/src/core/environment.js
@@ -1,12 +1,9 @@
 let backendUrlAuth = "https://api.qikaid.com";
 let backebdUrlApi = "https://api.qikaid.com";
-let backendUrlComprehend = "wss://fc19b4f9f05c.ngrok-free.app";
+let backendUrlComprehend = "https://api.qikaid.com";
 let backendUrlComprehendWebSocket = backendUrlComprehend + "/comprehend/ws/transcript?access_token=";
 let backendUrlAuthRefresh = backendUrlAuth + "/auth/token/refresh";
 let backendUrlApiUserConfig = backebdUrlApi + "/api/v1/user";
 let backendUrlApiQikAidBot = backebdUrlApi + "/api/v1/openai";
-
-    // this.backendUrl = 'wss://api.qikaid.com/comprehend/ws/transcript?access_token=';
-    // this.backendUrl = 'wss://5c00f7077846.ngrok-free.app/comprehend/ws/transcript?access_token=';
 
 export { backendUrlComprehendWebSocket, backendUrlAuthRefresh, backendUrlApiUserConfig , backendUrlApiQikAidBot};
